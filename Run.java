c89b27cc22789ea87d44d8fc74c4a3ab1393c99c
iyanskiii <131744423+iyanskiii@users.noreply.github.com>
2023-04-26 19:46:35 +0800

    Update Run.java

--- Run.java
+++ Run.java
@@ -1,6 +1,7 @@
 public class Run {
     public static void main (String[] args) {
         GameFrame frame = new GameFrame();
+        frame.connectToServer();
         frame.setUp();
         frame.addKeyBindings();
     }
