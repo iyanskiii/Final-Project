fb6022c74a5694139c9372fc2d597687c321b4c9
iyanskiii <131744423+iyanskiii@users.noreply.github.com>
2023-05-31 09:55:58 +0800

    Update Background.java

--- Background.java
+++ Background.java
@@ -1,3 +1,33 @@
+/*
+Brief Description of this Class:
+This class uses a Rectangle object to draw a background. This class also uses a Color object to draw a colored background 
+according to the theme.
+*/
+/**
+CSCI22 Final Project - Animated Scene
+@author Ronald Francis Bautista & Ian Roque Ferol
+@version May 15, 2023
+**/
+/*
+We have not discussed the Java language code in our program 
+with anyone other than our instructor/s or the teaching assistants 
+assigned to this course.
+We have not used Java language code obtained from another student, 
+or any other unauthorized source, either modified or unmodified.
+If any Java language code or documentation used in our program 
+was obtained from another source, such as a textbook or website, 
+that has been clearly noted with a proper citation in the comments 
+of our program.
+*/
+/*
+Certificate of Authorship:
+We hereby certify that the submission described in this document abides by 
+the principles stipulated in the DISCS Academic Integrity Policy document.
+We further certify that we are the authors of this submission and that any assistance 
+We received in its preparation is fully acknowledged and disclosed in the documentation.
+*/
+
+
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.event.*;
